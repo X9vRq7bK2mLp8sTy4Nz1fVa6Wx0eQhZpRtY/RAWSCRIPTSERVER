0b3c8f45982ae517f9195b3805e6e871336a4984
AMIXNSUJNJNSIXNSIHXNSUHBXEUHBUHNKSJN <jawshcx@icloud.com>
2025-08-22 19:39:58 +0200

    Update login.js

--- api/login.js
+++ api/login.js
@@ -1,14 +1,10 @@
-const jwt = require('jsonwebtoken');
-
-export default async function handler(req, res) {
+export default function handler(req, res) {
   const { username, password } = req.body;
   const expectedUsername = process.env.ADMIN_USERNAME;
   const expectedPassword = process.env.ADMIN_PASSWORD;
 
   if (username === expectedUsername && password === expectedPassword) {
-    const token = jwt.sign({ username }, process.env.JWT_SECRET, { expiresIn: '1h' });
-    res.status(200).json({ token });
-  } else {
-    res.status(401).json({ error: 'Invalid credentials' });
+    return res.status(200).json({ message: 'Login successful' });
   }
+  return res.status(401).json({ error: 'Invalid credentials' });
 }
