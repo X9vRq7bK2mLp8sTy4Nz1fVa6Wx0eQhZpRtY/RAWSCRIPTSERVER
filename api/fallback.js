66f43c6983b00687294879cdddab90862e0eeef8
AMIXNSUJNJNSIXNSIHXNSUHBXEUHBUHNKSJN <jawshcx@icloud.com>
2025-08-22 19:08:44 +0200

    Delete api directory

--- api/fallback.js
+++
@@ -1,4 +0,0 @@
-export default async function handler(req, res) {
-  res.setHeader('Content-Type', 'text/plain');
-  return res.status(404).send('Endpoint not found. Use /content?key=ROBLOX_SECRET_2024 to access content.');
-}
