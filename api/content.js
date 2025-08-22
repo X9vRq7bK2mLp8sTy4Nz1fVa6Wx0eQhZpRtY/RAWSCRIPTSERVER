b2d63f31f38d64beeb08c32122bdbd640ffb8d2c
AMIXNSUJNJNSIXNSIHXNSUHBXEUHBUHNKSJN <jawshcx@icloud.com>
2025-08-22 17:59:29 +0200

    Update content.js

--- api/content.js
+++ api/content.js
@@ -1,8 +1,20 @@
-// api/content.js - Serve content to whitelisted IPs
+// Import the shared content
 import { content } from './update.js';
 
+// Secret key - change this to something unique
+const SECRET_KEY = "ROBLOX_SECRET_2024";
+
 export default async function handler(req, res) {
-  // This will only be called for whitelisted IPs due to middleware
+  // Get the key from query parameters
+  const { key } = req.query;
+  
+  // Check if the key matches
+  if (key !== SECRET_KEY) {
+    res.setHeader('Content-Type', 'text/plain');
+    return res.status(403).send('Access Denied: Invalid key');
+  }
+  
+  // Valid key - serve the content
   res.setHeader('Content-Type', 'text/plain');
   return res.status(200).send(content);
 }
